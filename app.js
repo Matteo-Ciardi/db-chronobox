7387d024d670af3bc36f9a528b4bfda35b89bc3f
Damiana-Arangio <arangio.damiana@gmail.com>
2025-11-14 22:51:16 +0100

    Impostati router e controller degli ordini con CRUD (logica da implementare) + test eseguiti su Postman

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express')          // Import del modulo Express
 // Import dei router
 const capsulesRouter = require('./routers/capsulesRouter');
 const themesRouter = require('./routers/themesRouter');
-// const ordersRouter = require('./routers/ordersRouter');
+const ordersRouter = require('./routers/ordersRouter');
 // const paymentsRouter = require('./routers/paymentsRouter');
 
 
@@ -24,7 +24,7 @@ const port = 3000;                          // Definizione della porta su cui il
 // Registrazione dei router
 app.use('/api/capsules', capsulesRouter);
 app.use('/api/themes', themesRouter);      
-// app.use('/api/orders', ordersRouter);      
+app.use('/api/orders', ordersRouter);      
 // app.use('/api/payments', paymentsRouter); 
 
 
---
+++ routers/ordersRouter.js
@@ -0,0 +1,22 @@
+/************
+    IMPORT
+************/
+const express = require('express')                                  // Import Express
+const orderController = require('../controllers/orderController')   // Import Controller degli ordini
+
+/*************
+    ROUTER
+*************/
+const router = express.Router()                   // Inizializzazione router express
+
+// Definizione delle rotte CRUD
+router.get('/', orderController.index);           // Mostra tutte gli ordini
+router.get('/:id', orderController.show);         // Mostra un ordine specifico
+router.post('/', orderController.store);          // Crea un nuovo ordine
+router.put('/:id', orderController.update);       // Aggiorna un ordine
+router.delete('/:id', orderController.destroy);   // Elimina un ordine
+
+/************
+    EXPORT
+************/
+module.exports = router; // Export del router
